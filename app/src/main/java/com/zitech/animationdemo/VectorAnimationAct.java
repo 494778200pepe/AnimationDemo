82fe0b180354d9e5bdb154922d3b8b259da053e7
pepe <494778200@qq.com>
2016-09-13 11:23:12 +0800

    VectorAnimation

--- app/src/main/java/com/zitech/animationdemo/VectorAnimationAct.java
+++ app/src/main/java/com/zitech/animationdemo/VectorAnimationAct.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.zitech.animationdemo.Vector.VectorDemo1Act;
+import com.zitech.animationdemo.Vector.VectorDemo2Act;
+import com.zitech.animationdemo.Vector.VectorDemo3Act;
 
 /**
  * Created by pepe on 2016/8/19 0019.
@@ -26,8 +28,10 @@ public class VectorAnimationAct extends AppCompatActivity implements View.OnClic
                 startActivity(new Intent(this,VectorDemo1Act.class));
                 break;
             case R.id.main_btn1://
+                startActivity(new Intent(this,VectorDemo2Act.class));
                 break;
             case R.id.main_btn2://
+                startActivity(new Intent(this,VectorDemo3Act.class));
                 break;
             case R.id.main_btn3://
                 break;
